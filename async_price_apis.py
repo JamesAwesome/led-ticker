6683c18bab32a39bbc834aad1c60db4827382035
James Awesome <james@morelli.nyc>
2021-09-26 14:49:50 -0400

    pylint cleanup

--- async_price_apis.py
+++ async_price_apis.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3 -u
+"""Async Price APIs
 
-import sys
+Async price monitor widgets
+"""
+
 import itertools
 import asyncio
-import aiohttp
 import logging
 import json
 from datetime import date, timedelta
 
+import aiohttp
 import attr
-from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+from rgbmatrix import graphics
 
 
 FONT_SYMBOL = graphics.Font()
@@ -32,47 +35,83 @@ DOWN_TREND_COLOR = graphics.Color(194, 24, 7)
 COINBASE_API = "https://api.coinbase.com"
 
 
+def _get_change_width(font_change, change_word, padding=6):
+    """get the width of font text + padding
+    """
+    change_width = (
+        sum([font_change.CharacterWidth(ord(c)) for c in change_word]) + padding
+    )
+
+    return change_width
+
+
+def _get_change_color(change_str):
+    """choose the color for the price change
+    """
+    if change_str.startswith("-"):
+        return DOWN_TREND_COLOR
+
+    return UP_TREND_COLOR
+
+def _get_price_font(price_str):
+    """choose big or small font for the price
+    """
+    if len(price_str) > 10:
+        return FONT_PRICE_SMALL
+
+    return FONT_PRICE
+
+
 @attr.s
-class AsyncPriceMonitor(object):
+class AsyncPriceMonitor():
+    """An asynchronous crypto monitor widget to be used with AsyncTicker
+
+    Uses coinbases API
+    """
     symbol = attr.ib(type=str)
     currency = attr.ib(type=str)
     session = attr.ib()
-    update_interval = attr.ib(type=int, default=300)
     price = attr.ib(type=float, init=False)
     yesterdays_price = attr.ib(type=float, init=False)
     change_24h = attr.ib(type=float, init=False)
-    change_str = attr.ib(type=str, init=False)
-    price_str = attr.ib(type=str, init=False)
+    spot_url = attr.ib(type=str, init=False)
+
 
     def __attrs_post_init__(self):
         self.spot_url = f"{COINBASE_API}/v2/prices/{self.symbol}-{self.currency}/spot"
 
     @classmethod
-    async def start(cls, symbol, currency, session, update_interval):
-        price_monitor = await cls(symbol, currency, session, update_interval).update()
-        asyncio.create_task(price_monitor.monitor())
+    async def start(cls, symbol, currency, session, update_interval=300):
+        """init and run this monitor
+        """
+        price_monitor = await cls(symbol, currency, session).update()
+        asyncio.create_task(price_monitor.monitor(update_interval))
         return price_monitor
 
-    async def monitor(self):
+    async def monitor(self, update_interval):
+        """update self in a loop
+        """
         while True:
-            await asyncio.sleep(self.update_interval)
+            await asyncio.sleep(update_interval)
             await self.update()
 
     async def update(self):
-        logging.info(f"Updating monitor for {self.symbol}")
+        """update price information
+        """
+        logging.info("Updating monitor for %s", self.symbol)
         self.price = await self.get_spot_price()
-        self.price_str = f"{self.price:.4f}"
 
         self.yesterdays_price = await self.get_yesterdays_price()
 
         self.change_24h = (
             (self.price - self.yesterdays_price) / self.yesterdays_price
         ) * 100
-        self.change_str = f"{self.change_24h:.2f}%"
 
         return self
 
     async def get_spot_price(self, spot_date=None):
+        """get a spot price from the coinbase api, defaults to current
+        """
         params = {}
 
         if spot_date:
@@ -85,58 +124,49 @@ class AsyncPriceMonitor(object):
         return spot_price
 
     async def get_yesterdays_price(self):
+        """get yesterdays spot price from the coinbase api
+        """
         yesterday = date.today() - timedelta(days=1)
         yesterdays_price = await self.get_spot_price(spot_date=yesterday)
 
         return yesterdays_price
 
-    def _get_change_width(self, font_change, change_word, padding=6):
-        change_width = (
-            sum([font_change.CharacterWidth(ord(c)) for c in change_word]) + padding
-        )
 
-        return change_width
-
-    def _get_change_color(self, change_str):
-        if change_str.startswith("-"):
-            return DOWN_TREND_COLOR
-
-        return UP_TREND_COLOR
-
-    def _get_price_font(self, price_str):
-        if len(price_str) > 10:
-            return FONT_PRICE_SMALL
-
-        return FONT_PRICE
 
     def draw(self, canvas, cursor_pos=3):
+        """draw this monitor to a canvas
+        """
+        change_str = f"{self.change_24h:.2f}%"
+        price_str = f"{self.price:.4f}"
 
-        change_color = self._get_change_color(self.change_str)
-        font_price = self._get_price_font(self.price_str)
+        change_color = _get_change_color(change_str)
+        font_price = _get_price_font(price_str)
 
         # Draw the elements on the canvas
         graphics.DrawText(
             canvas, FONT_SYMBOL, cursor_pos, 12, DEFAULT_COLOR, self.symbol
         )
 
-        price_x = cursor_pos + self._get_change_width(FONT_SYMBOL, self.symbol)
+        price_x = cursor_pos + _get_change_width(FONT_SYMBOL, self.symbol)
 
         graphics.DrawText(
-            canvas, font_price, price_x, 12, DEFAULT_COLOR, self.price_str
+            canvas, font_price, price_x, 12, DEFAULT_COLOR, price_str
         )
 
-        change_x = price_x + self._get_change_width(font_price, self.price_str)
+        change_x = price_x + _get_change_width(font_price, price_str)
 
         graphics.DrawText(
-            canvas, FONT_CHANGE, change_x, 12, change_color, self.change_str
+            canvas, FONT_CHANGE, change_x, 12, change_color, change_str
         )
 
-        cursor_pos = change_x + self._get_change_width(FONT_CHANGE, self.change_str)
+        cursor_pos = change_x + _get_change_width(FONT_CHANGE, change_str)
 
         return canvas, cursor_pos
 
 
 async def print_value(price_monitors):
+    """test print values to stdout
+    """
     while True:
         for price_monitor in itertools.cycle(price_monitors):
             print(
@@ -154,6 +184,8 @@ async def print_value(price_monitors):
 
 
 async def main():
+    """test run some monitors
+    """
     async with aiohttp.ClientSession() as session:
 
         price_monitors = await asyncio.gather(

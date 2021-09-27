45b3060b66dd58025a0543825675a88c17950c23
James Morelli <james@morelli.nyc>
2021-09-27 12:59:44 -0400

    try coingecko

--- main.py
+++ main.py
@@ -36,11 +36,11 @@ async def main(coinbase_symbols, coingecko_symbols):
     async with aiohttp.ClientSession() as session:
         monitors = []
 
-        monitors.extend(await start_coingecko_monitors(coingecko_symbols, 'USD', session))
-
         monitors.extend([
             await CoinbasePriceMonitor.start(symbol, "USD", session) for symbol in coinbase_symbols
         ])
+
+        monitors.extend(await start_coingecko_monitors(coingecko_symbols, 'USD', session))
 
         gas_price_monitor = await EtherscanGasMonitor.start(
             session, api_key=os.getenv("ETHERSCAN_API_KEY")
@@ -55,6 +55,6 @@ if __name__ == "__main__":
     asyncio.run(
         main(
             ["ETH", "BTC", "XLM", "SOL", "ADA", "COMP", "SUSHI"],
-            ["ORCA"]
+            ["ORCA", "SAMO"]
         )
     )

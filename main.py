a2932b0ca90e4e2a5dae57a30dd60703f34c8cf6
James Awesome <james@morelli.nyc>
2021-09-27 22:29:04 -0400

    switch on scroll type

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def main(coinbase_symbols, coingecko_symbols):
 
         monitors.append(gas_price_monitor)
 
-        await AsyncTicker(monitors, led_frame).run_forever_scroll()
+        await AsyncTicker(monitors, led_frame).run_swap()
 
 
 if __name__ == "__main__":

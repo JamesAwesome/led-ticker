50a0d13bc9b7eb49bceaebbfc3825196ed1064c9
James Morelli <james@morelli.nyc>
2021-09-28 18:25:54 -0400

    fuck around

--- async_widgets.py
+++ async_widgets.py
@@ -63,7 +63,7 @@ class TickerMessage:
             else:
                 center_pos = _find_center(canvas, change_width)
                 cursor_pos += center_pos
-                end_padding = (canvas.width - (center_pos + change_width)) * whitespace_width
+                end_padding = math.floor(canvas.width - (center_pos + change_width)) * whitespace_width
 
         cursor_pos += graphics.DrawText(
             canvas, self.font, cursor_pos, 12, self.font_color, self.message + (' ' * end_padding)

73f39b37a5c2e40fb469068369ebec57e3ca3829
James Morelli <james@morelli.nyc>
2021-10-01 16:00:43 -0400

    refactor run_swap

--- main.py
+++ main.py
@@ -56,9 +56,10 @@ async def main(coinbase_symbols, coingecko_symbols):
 
         while True:
 
-            await AsyncRSSFeedTicker(
-                feed_monitor,
+            await AsyncTicker(
+                feed_monitor.feed_stories,
                 led_frame,
+                title=feed_monitor.feed_title
                 title_delay=5,
             ).run_forever_scroll(loop_count=1)
 
@@ -66,7 +67,7 @@ async def main(coinbase_symbols, coingecko_symbols):
                 monitors,
                 led_frame,
                 title=TickerMessage('* Crypto Prices *'),
-            ).run_swap(loop_count=2)
+            ).run_forever_scroll(loop_count=2)
 
 
 if __name__ == "__main__":

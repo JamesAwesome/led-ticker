eb4a5880bb66ec392809ccedff5fa6625075d19e
James Awesome <james@morelli.nyc>
2021-10-01 21:35:09 -0400

    Cycle feeds

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ from frame import LedFrame
 
 
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
+handler.setLevel(logging.INFO)
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)

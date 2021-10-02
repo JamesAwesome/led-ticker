1167d3747bedb84ada8070313e813febb6c6cc51
James Awesome <james@morelli.nyc>
2021-10-01 21:41:18 -0400

    limit feeds

--- async_news_feed.py
+++ async_news_feed.py
@@ -82,9 +82,9 @@ class RSSFeedMonitor:
             feed_data = await response.text()
             feed = feedparser.parse(feed_data)
             self.feed_title = TickerMessage(feed['channel']['title'], font_color=next(self.colors))
-            self.feed_stories = list([
+            self.feed_stories = itertools.islice([
                 TickerMessage(item['title'], font_color=next(self.colors)) for item in feed['items']
-            ])[:self.max_stories]
+            ], self.max_stories)
             logging.info('feed story count: %s', len(self.feed_stories))
 
         return self

7e1e20f1c681a6f56badf870cfe39c1141e542ad
James Awesome <james@morelli.nyc>
2021-10-07 17:16:27 -0400

    tests work

--- tests/test_async_ticker.py
+++ tests/test_async_ticker.py
@@ -12,16 +12,26 @@ import async_ticker
 @pytest.mark.asyncio
 async def test__enque_ticker_objects():
     test_queue = asyncio.Queue()
-    test_iter = iter(range(0, 5))
+    test_items = list(range(0, 5))
+    test_iter = iter(test_items)
 
+    # Create a task to enqueue items onto our queue
     asyncio.create_task(
         async_ticker._enque_ticker_objects(test_iter, test_queue)
     )
 
-    # Consume the same amount of items from the queue
-    while not test_queue.empty():
-        item = await test_queue.get()
-        assert item
+    # Make a list to store our results
+    res_items = []
 
-    # Ensure the queue is now empty
-    assert test_queue.empty()
+    # Leave some time to make sure the next object is on the queue
+    res_items.append(await test_queue.get())
+
+    # Get items until the queue is empty
+    while not test_queue.empty():
+        res_item = await test_queue.get()
+        res_items.append(res_item)
+
+        # Leave time for the next object to make it onto the queue
+        await asyncio.sleep(0.05)
+
+    assert res_items == test_items

df6a9680b1ec2be0437adb5b02c33b6121bd15ce
James Awesome <james@morelli.nyc>
2021-10-31 21:53:30 -0400

    days since thanksgiving

--- scripts/crypto-ticker.py
+++ scripts/crypto-ticker.py
@@ -13,7 +13,7 @@ import aiohttp
 from async_ticker.async_price_apis import CoinbasePriceMonitor, EtherscanGasMonitor, start_coingecko_monitors
 from async_ticker.async_news_feed import RSSFeedMonitor
 from async_ticker.async_ticker import AsyncTicker
-from async_ticker.widgets import TickerMessage
+from async_ticker.widgets import TickerMessage, TickerCountdown
 from async_ticker.frame import LedFrame
 from async_ticker.colors import ORANGE, UP_TREND_COLOR
 
@@ -100,7 +100,7 @@ async def main(coinbase_symbols, coingecko_symbols):
 
             await AsyncTicker(
                 [
-                    TickerMessage('Happy Halloween!', font_color=ORANGE)
+                    TickerCountdown('Days Until Thanksgiving', font_color=ORANGE)
                 ],
                 led_frame,
                 notif_queue=notif_queue,
--- src/async_ticker/widgets.py
+++ src/async_ticker/widgets.py
@@ -4,6 +4,8 @@
 Async price monitor widgets
 """
 
+from datetime import date
+
 import attr
 from rgbmatrix import graphics
 
@@ -46,6 +48,45 @@ class TickerMessage:
 
         cursor_pos += graphics.DrawText(
             canvas, self.font, cursor_pos, 12, font_color, self.message
+        )
+
+        cursor_pos += end_padding
+
+        return canvas, cursor_pos
+
+@attr.s
+class TickerCountdown:
+    """An generic countdown"""
+
+    message = attr.ib(type=str)
+    countdown_date = attr.ib()
+    font = attr.ib(default=FONT_DEFAULT)
+    font_color = attr.ib(default=DEFAULT_COLOR)
+    center = attr.ib(default=True)
+    padding = attr.ib(type=int, default=6)
+
+    def draw(self, canvas, cursor_pos=0, font_color=None, **kwargs):
+        """draw this monitor to a canvas"""
+        # Draw the elements on the canvas
+        today = date.today()
+        days_until = (today - self.countdown_date).days
+
+        font_color = font_color if font_color else self.font_color
+
+        change_width = get_text_width(self.font, self.message, padding=0)
+        end_padding = self.padding
+
+        if self.center:
+            if change_width > canvas.width:
+                cursor_pos = cursor_pos
+
+            else:
+                center_pos = find_center(canvas, change_width)
+                end_padding = canvas.width - (center_pos + change_width)
+                cursor_pos += center_pos
+
+        cursor_pos += graphics.DrawText(
+            canvas, self.font, cursor_pos, 12, font_color, f'{self.message}: {days_until}'
         )
 
         cursor_pos += end_padding
